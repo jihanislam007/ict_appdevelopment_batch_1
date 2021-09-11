fa0961c942f6d6422a6b35809389a487604206d9
Durbin <jihanislam007@gmail.com>
2021-09-11 23:02:38 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Hafizur());
+}
+
+class Hafizur extends StatelessWidget {
+  const Hafizur({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.deepOrangeAccent,
+          elevation: 40,
+          leading: Icon(Icons.ac_unit_sharp),
+          centerTitle: true,
+          title: Text("Title"),
+          actions: [Icon(Icons.access_time)],
+        ),
+        body: SafeArea(
+            child:
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Center(
+                child: Text("Bangladesh, to the east of India on the Bay of Bengal, is a South Asian country marked by lush greenery and many waterways.\n\nIts Padma (Ganges), Meghna and Jamuna rivers create fertile plains, and travel by boat is common.\n\n\t\tOn the southern coast, the Sundarbans, an enormous mangrove forest shared with Eastern India, is home to the royal Bengal tiger.",
+                style: TextStyle(
+                  color: Colors.purple,
+                  fontSize: 20,
+                  decoration: TextDecoration.underline,
+                  decorationColor: Colors.red,
+                  decorationStyle: TextDecorationStyle.wavy,
+
+
+                ),),
+              ),
+            )
+        ),
+      ),
+    );
+  }
+}
