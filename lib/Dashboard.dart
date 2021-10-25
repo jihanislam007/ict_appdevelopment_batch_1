d99af854f84ccf455ff13c5fc21be1e3269ce833
Durbin <jihanislam007@gmail.com>
2021-10-25 13:08:32 +0600

    Class 24

--- lib/Dashboard.dart
+++ lib/Dashboard.dart
@@ -2,22 +2,68 @@ import 'package:flutter/material.dart';
 
 import 'Cards.dart';
 import 'Containers.dart';
+import 'Expandeds.dart';
+import 'Flexibles.dart';
+import 'Grideview.dart';
 import 'RowColumn.dart';
 import 'Stacks.dart';
 import 'imageLoad.dart';
 import 'listTiles.dart';
+import 'mediaquery.dart';
 
 class Dashboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.teal
+                ),
+                child: CircleAvatar(radius: 50,)
+            ),
+
+            ListTile(
+              leading: Icon(Icons.account_balance),
+              title: Text("Home"),
+              onTap: (){
+                print("i am home");
+              },
+            ),
+
+            ListTile(
+              leading: Icon(Icons.account_balance),
+              title: Text("Home"),
+            )
+            ,
+            ListTile(
+              leading: Icon(Icons.account_balance),
+              title: Text("Home"),
+            ),
+            ListTile(
+              leading: Icon(Icons.account_balance),
+              title: Text("card page"),
+              onTap: () {
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => cards()));
+              }
+            )
+
+          ],
+        ),
+
+      ),
+
         appBar: AppBar(
           title: Text("Batch -01"),
         ),
         body: SafeArea(
           child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+            child: ListView(
+              //mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Text(" cbvdhcbdjcdk"),
                 Text(" cbvdhcbdjcdk"),
@@ -55,7 +101,32 @@ class Dashboard extends StatelessWidget {
                     onPressed: (){
                       Navigator.push(context, MaterialPageRoute(builder: (context) => cards()));
                     },
-                    child: Text("Card"))
+                    child: Text("Card")),
+
+                ElevatedButton(
+                    onPressed: (){
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => gridviews()));
+                    },
+                    child: Text("GridView")),
+
+                ElevatedButton(
+                    onPressed: (){
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => Expandeds()));
+                    },
+                    child: Text("Expandeds")),
+
+                ElevatedButton(
+                    onPressed: (){
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => flexibles()));
+                    },
+                    child: Text("Flexible")),
+
+                ElevatedButton(
+                    onPressed: (){
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => mediaQuerys()));
+                    },
+                    child: Text("Media Query"))
+
 
               ],
             ),
